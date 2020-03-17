0274a5390a6e6dda6292e31d8bc9408a52f2dd1b
fimorr <fimorrr@yandex.ru>
2020-03-17 16:47:23 +0300

    Updates

---
+++ precache-manifest.ab66a61b2be76604496487fb05d311f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac1182e03ecf19f18ffc3b32283d941",
+    "url": "/consultation/index.html"
+  },
+  {
+    "revision": "14bc4dee3c15b8d51e34",
+    "url": "/consultation/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "e7644afd862c2a7cd426",
+    "url": "/consultation/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "14bc4dee3c15b8d51e34",
+    "url": "/consultation/static/js/2.4ed14a52.chunk.js"
+  },
+  {
+    "revision": "e7644afd862c2a7cd426",
+    "url": "/consultation/static/js/main.cd477965.chunk.js"
+  },
+  {
+    "revision": "30c6f695479a4c568236",
+    "url": "/consultation/static/js/runtime-main.091204e6.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/consultation/static/media/persik.4e1ec840.png"
+  }
+]);
